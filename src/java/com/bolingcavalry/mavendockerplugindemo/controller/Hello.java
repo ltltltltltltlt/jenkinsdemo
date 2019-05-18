660f8d1386d506e0f608c83c1d3465e20e2b1ecc
e <lijinming0214@126.com>
2019-05-18 15:19:03 +0800

    commit new

--- src/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@RestController
-public class Hello {
+@RestController 
+public class Hello { 
 
-    @RequestMapping("/")
-    public String sayHello(){
-    	//修改返回的字符串的内容
-        return "123456. Hello jenkins, " + new Date();
-    }
+	@RequestMapping("/") 
+	public String sayHello(){ 
+	//修改返回的字符串的内容 
+	return "abcdefg. Hello jenkins, " + new Date(); 
+	} 
 }
